5d6728906dd77443c6cd5a96924a5f1c9b53082e
r-telang <114829792+r-telang@users.noreply.github.com>
2023-10-05 14:10:33 -0400

    Feature/yml fix (#8)
    * changes in yml
    
    * changes in database file

--- config/database.js
+++ config/database.js
@@ -3,20 +3,20 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: "mysql",
-//   }
-// );
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+  }
+);
 
-const sequelize = new Sequelize("demo", "root", "Rajas@281097", {
-  host: "localhost",
-  dialect: "mysql",
-});
+// const sequelize = new Sequelize("demo", "root", "Rajas@281097", {
+//   host: "localhost",
+//   dialect: "mysql",
+// });
 
 // console.log(process.env.DB_USER);
 export default sequelize;
