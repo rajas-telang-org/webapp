65762f692870045905becf29bc03d1d95613f6c5
r-telang <telang.r@northeastern.edu>
2023-10-25 15:08:16 -0400

    changes in script-1

--- script/setup_script.sh
+++ script/setup_script.sh
@@ -15,9 +15,6 @@ sudo apt install -y nodejs npm unzip
 #creting user group
 sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
-
-sudo cp csye6225.service /etc/systemd/system
-
 # Move webapp.zip and .env to /opt/
 cd /tmp
 sudo mv /tmp/webapp.zip /opt/csye6225/webapp.zip
@@ -38,8 +35,9 @@ sudo apt update
 # Unzip webapp.zip
 sudo unzip webapp.zip
 
+sudo cp csye6225.service /etc/systemd/system
 sudo chown -R csye6225:csye6225 /opt/csye6225
-sudo chmod +x /opt/csye6225/app.js
+sudo chmod +x /opt/csye6225/index.js
 
 sudo npm install
 
@@ -48,12 +46,6 @@ sudo systemctl enable csye6225
 sudo systemctl start csye6225
 sudo systemctl restart csye6225
 sudo systemctl stop csye6225
-
-
-
-
-
-
 
 # Install Node.js dependencies
 sudo npm install
