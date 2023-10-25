40ce2b413b36ed5f0cd493a4e7ca3f474c53ea75
r-telang <telang.r@northeastern.edu>
2023-10-25 13:36:11 -0400

    changes in workflow.

--- script/setup_script.sh
+++ script/setup_script.sh
@@ -10,9 +10,13 @@ sudo apt-get upgrade -y
 # Clean up
 sudo apt-get clean
 
+#creting user group
+sudo groupadd csye6225
+sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
+sudo cp csye6225.service /etc/systemd/system
+
 # Move webapp.zip and .env to /opt/
-sudo mv /tmp/webapp.zip /opt/webapp.zip
-sudo mv /tmp/.env /opt/.env
+sudo mv /tmp/webapp.zip /opt/csye6225/webapp.zip
 
 # Install unzip
 sudo apt install -y unzip
@@ -29,12 +33,6 @@ sudo apt-get purge mariadb-server
 # Update package lists
 sudo apt update
 
-# Install Mariadb-server
-sudo apt install -y mariadb-server
-
-# Start and enable Mariadb service
-sudo systemctl start mariadb
-sudo systemctl enable mariadb
 
 # Unzip webapp.zip
 sudo unzip webapp.zip
@@ -42,10 +40,8 @@ sudo unzip webapp.zip
 # Install Node.js dependencies
 sudo npm install
 
-sudo groupadd csye6225
-sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
-sudo cp csye6225.service /etc/systemd/system
-systemctl daemon-reload
+
+sudo systemctl daemon-reload
 sudo systemctl enable csye6225
 sudo systemctl start csye6225
 sudo systemctl restart csye6225
