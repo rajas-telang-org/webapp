9105d1e9daff12cedd9e79357337c6b021ca272a
r-telang <114829792+r-telang@users.noreply.github.com>
2023-10-25 14:49:04 -0400

    changes in script (#9)

--- script/setup_script.sh
+++ script/setup_script.sh
@@ -10,25 +10,26 @@ sudo apt-get upgrade -y
 # Clean up
 sudo apt-get clean
 
+sudo apt install -y nodejs npm unzip
+
 #creting user group
 sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
+
 sudo cp csye6225.service /etc/systemd/system
 
 # Move webapp.zip and .env to /opt/
+cd /tmp
 sudo mv /tmp/webapp.zip /opt/csye6225/webapp.zip
 
-# Install unzip
-sudo apt install -y unzip
+
 
 # Change directory to /opt/
 cd /opt/csye6225
 
-# Install Node.js and npm
-sudo apt install -y nodejs npm
 
 # Purge Mariadb-server
-sudo apt-get purge mariadb-server
+# sudo apt-get purge mariadb-server
 
 # Update package lists
 sudo apt update
@@ -37,12 +38,24 @@ sudo apt update
 # Unzip webapp.zip
 sudo unzip webapp.zip
 
-# Install Node.js dependencies
-sudo npm install
+sudo chown -R csye6225:csye6225 /opt/csye6225
+sudo chmod +x /opt/csye6225/app.js
 
+sudo npm install
 
 sudo systemctl daemon-reload
 sudo systemctl enable csye6225
 sudo systemctl start csye6225
 sudo systemctl restart csye6225
 sudo systemctl stop csye6225
+
+
+
+
+
+
+
+# Install Node.js dependencies
+sudo npm install
+
+
